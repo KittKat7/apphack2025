35b3a8b843644785a0888a3a90e63c89e524c16f
schoi88 <seanchoi2239@gmail.com>
2025-04-12 15:03:56 -0400

    world

---
+++ world.py
@@ -0,0 +1,11 @@
+from gameobject import GameObject
+
+class World:
+    def __init__(self) -> None:
+        worldMap[][]: GameObject
+        x: int 
+        y: int 
+        foodGrowthRate: float
+        foodQuality: int
+        geneRandomness: float
+        
