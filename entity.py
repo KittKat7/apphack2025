9daeecf1fd5400f410b32a991eba9c2ab8255160
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-04-12 14:04:25 -0400

    Entity base

---
+++ entity.py
@@ -0,0 +1,9 @@
+
+class Entity:
+    def __init__(self) -> None:
+        speed: float
+        stamina: float
+        perception: float
+        strength: float
+        toughness: float
+        lifespan: float
