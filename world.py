aa8c265bed2bd22f86ad1bf92b3fd8245a549891
schoi88 <seanchoi2239@gmail.com>
2025-04-12 16:37:38 -0400

    small typing changes

--- world.py
+++ world.py
@@ -1,11 +1,10 @@
 from gameobject import GameObject
+from entity import Entity
 
 class World:
     def __init__(self) -> None:
-        worldMap: list[list[GameObject]]
-        x: int 
-        y: int 
-        foodGrowthRate: float
-        foodQuality: int
-        geneRandomness: float
-        
+        self.worldMap: list[list[GameObject]]
+        self.gameObjects: dict[Entity, tuple(int, int)]
+        self.foodGrowthRate: float
+        self.foodQuality: int
+        self.geneRandomness: float
