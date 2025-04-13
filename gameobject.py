39c20446e9c5a8742c3c52f7b0276af58d353af8
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-04-12 23:40:21 -0400

    Update gameobject

--- gameobject.py
+++ gameobject.py
@@ -1,18 +1,6 @@
-import game as Game
 class GameObject:
     """
     """ 
-    def __init__(self, type:str, x:int=None, y:int=None)->None:
-        self:GameObject = self
-        self.type:str = type
-        self.x:int = x
-        self.y:int = y
-    
-    def getPos(self)->tuple[int, int]:
-        x, y = Game.getGameObjectPos(self)
-        self.x = x
-        self.y = y
-        return (x,y)        
 
 class Food(GameObject):
     """
