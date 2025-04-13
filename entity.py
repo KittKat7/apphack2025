0b1e6114ba9dac68602e9c7707ca3d903f346b60
zach <stpierrezd@appstate.edu>
2025-04-13 03:27:45 -0400

    entitiy additions

--- entity.py
+++ entity.py
@@ -18,12 +18,13 @@ class Entity(GameObject):
         observableWorld:list[list[GameObject]] = self.perceive()
         
         #look for things nearby
-        entities:dict = dict()
-        foods:dict = dict()
+        entities:dict[Entity, tuple[int,int]] = dict()
+        foods:dict[Food, tuple[int,int]] = dict()
+        gameObjects:dict[GameObject, tuple[int,int]] = dict()
         obsWrldHeight:int = len(observableWorld)
         obsWrldWidth:int = len(observableWorld[0])
         
-        rawPerception:int = int(self.perception*maxPerception)
+        rawPerception:int = round(self.perception*maxPerception)
         
         #find nearby things [..., self, ....]
         for i in range(0,rawPerception+1+rawPerception):
@@ -33,13 +34,51 @@ class Entity(GameObject):
                 tmp = observableWorld[i][j]
                 if isinstance(tmp, Entity):
                     entities.update({tmp:(i,j)})
+                    gameObjects.update({tmp:(i,j)})
                 elif isinstance(tmp, Food):
                     foods.update({tmp:(i,j)})
+                    gameObjects.update({tmp:(i,j)})
                 else:
                     pass #don't care abt empty spaces
+                
+                
+        #Decide which of the following scenarios are happening
+            #first priority is surviving so if you see something with strength greater than your toughness run
+            #second priority is eating things with a lower toughness, than your strength
+            #third priority is eating food scattered around the world
+            #lastly if you don't see anything move around randomly
             
-        #first priority is surviving so if you see something with strength greater than your toughness run
-        #second priority is getting food, if you see food or 
+        run:bool = False
+        chase:bool = False
+        gettingFood:bool = False
+        movingRandomly:bool = True #default action
         
-        
+        for entity in entities:
+            if entity.strength > self.toughness: #run away
+                run = True
+                chase = False
+                gettingFood = False
+                movingRandomly = False
+                break #don't need to check anymore if running
+            elif entity.toughness <= self.strength and not run:
+                chase = True 
+                gettingFood = False
+                movingRandomly = False
+             
+        if not run and not chase:  
+            if len(foods) > 0: #entity can see food
+                gettingFood = True
+                movingRandomly = False
+                    
+        if not run and not chase and not gettingFood: #redundant check 
+            movingRandomly = True                    
+            
+        #do actions based on what was decided
+        if run:
+            pass #TODO
+        elif chase:
+            pass #TODO
+        elif gettingFood:
+            pass #TODO
+        else: #moves randomly
         
